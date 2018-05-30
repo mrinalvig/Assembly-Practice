118223654111068c7ffb9051bc2e4bf451950873
Mrinal Vig <mrinalvig@hotmail.com>
2018-05-29 23:41:30 -0700

    Corrected Assignment

--- Assignment 11.asm
+++ Assignment 11.asm
@@ -1,24 +1,66 @@
-.386
-.model flat,stdcall
-.stack 4096
-ExitProcess proto,dwExitCode:dword
+ExitProcess Proto
+ReadInt64 Proto
+WriteInt64 Proto
+Crlf Proto
+
 .data
-array dword 1,2,3,4,5,6,7,8
+MyArray QWORD 10d, 20d, 30d, 40d
+FinalArray QWORD 4 DUP (0) 
+
 .code
 main proc
-  mov esi, OFFSET array
-  mov ecx, LENGTHOF array -1
-    
-myLoop:
-  mov ebx, [esi]
-  mov edx, [esi + 4]
- 
-  mov [esi], edx
-  mov [esi + 4], ebx
-  add esi, TYPE array
- 
-loop myLoop
- 
-  invoke ExitProcess,0
+
+mov rsi, 0
+mov r15, 8 
+mov rcx, 4
+mov rsi, 0
+
+  startLoop:
+
+  mov rax, rsi ; i = 0
+  mul r15 ; i = i * 8
+  mov rdi, rax
+  mov rax, MyArray[rdi] ; var = MyArray[i]
+
+  call WriteInt64 
+
+  inc rsi ;i++
+
+Loop startLoop
+
+	call Crlf
+	mov rcx, lengthof myArray / 2 ; sets # of loops to half the array length
+  mov rsi, 0
+  
+	MyLoop:
+  
+	mov rax, rsi
+  mul r15 
+	mov rdx, qword ptr myArray[rax]; rdx has current value 
+	mov r14,  qword ptr myArray[rax + 8]; r14 has current value + 1
+	mov FinalArray [rax + 8], rdx ; sets i+1 = i 
+	mov FinalArray [rax], r14 ; i = i+1
+	inc rsi ; increments twice so its always odd switching with even
+	inc rsi
+
+	Loop MyLoop
+  
+	mov rcx, 4
+  mov rsi, 0
+  
+	finalLoop:
+
+	mov rax, rsi 
+	mul r15 
+	mov rdi, rax
+	mov rax, FinalArray[rdi] 
+	call WriteInt64 
+	inc rsi 
+
+	Loop finalLoop
+
+	call ReadInt64
+	call ExitProcess
+
 main endp
-end main
+end
